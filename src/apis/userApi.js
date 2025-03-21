ec9598a2d0d5e41d01987c9670902c7ba5a0a254
Nguyen Lam <nlsc1999@gmail.com>
2025-03-21 17:11:10 +0700

    feat: implement user API slice and integrate with Redux store

---
+++ src/apis/userApi.js
@@ -0,0 +1,48 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// Tạo API slice
+export const userApi = createApi({
+  reducerPath: 'userApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_API,
+    credentials: 'include',
+  }),
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => 'users',
+    }),
+
+    login: builder.mutation({
+      query: (data) => ({
+        url: 'login',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+
+    addUser: builder.mutation({
+      query: (data) => ({
+        url: 'users',
+        method: 'POST',
+        body: data,
+      }),
+    }),
+
+    updateUser: builder.mutation({
+      query: ({ id, ...data }) => ({
+        url: `users/${id}`,
+        method: 'PATCH',
+        body: data,
+      }),
+    }),
+
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `users/${id}`,
+        method: 'DELETE',
+      }),
+    }),
+  }),
+});
+
+export const { useGetUsersQuery, useAddUserMutation, useUpdateUserMutation, useDeleteUserMutation } = userApi;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './reducers/user';
+import { userApi } from '../apis/userApi';
 
 export default configureStore({
   reducer: {
+    [userApi.reducerPath]: userApi.reducer,
     user: userReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(userApi.middleware),
 });
