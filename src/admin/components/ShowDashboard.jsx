5c031287ef962a9b02e770a15cc4337a9418f182
HuwngThanh <hungthanhvuong22102004@gmail.com>
2025-05-25 17:08:19 +0700

    End.

--- src/admin/components/ShowDashboard.jsx
+++ src/admin/components/ShowDashboard.jsx
@@ -1,33 +1,14 @@
-import React, { useEffect, useState } from "react";
-import Navbar from './js/navbar';
-import Siderbar from './js/siderbar';
-import Dashboard from './js/Dashboard'
-import { GetListMoviesTop } from "../../apis/moviesApi.js";
+import React, { useEffect, useState } from 'react';
+import Navbar from './js/navbar.jsx';
+import Siderbar from './js/siderbar.jsx';
+import Dashboard from './js/Dashboard.jsx';
 
 function ShowDashboard() {
-  const [movies, setMovies] = useState([]);
-
-
-
-  useEffect(() => {
-          GetListMoviesTop({})
-              .then((response) => {
-                  console.log("🚀 ~ fetchMovies ~ response:", response);
-                  if (response?.data) {
-                      setMovies(response.data); // Lưu dữ liệu phim vào state
-                  }
-              })
-              .catch((err) => {
-                  console.error("🚀 ~ GetListMovies ~ err:", err);
-              });
-      }, []);
-      
-
   return (
     <>
-        <Navbar></Navbar>
-        <Siderbar></Siderbar>
-        <Dashboard movies={movies} ></Dashboard>
+      <Navbar></Navbar>
+      <Siderbar></Siderbar>
+      <Dashboard></Dashboard>
     </>
   );
 }
